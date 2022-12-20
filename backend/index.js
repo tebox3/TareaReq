accd4e376069e0bbdec52638ddca233474a7962f
Pancho <tebox007@gmail.com>
2022-12-19 23:16:38 -0300

    tarea

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 import express  from "express";
-import cors from 'cors' 
-import routerProyecto from './src/routes/informacion.routes.js'
+import cors from 'cors';
+import routerProyecto from './src/routes/informacion.routes.js';
+import comision from "./src/routes/comision.routes.js"; 
+import disponibilidad from "./src/routes/disponibilidad.routes.js";
+import estudiantes from "./src/routes/estudiantes.routes.js"
+import examen from "./src/routes/examen.routes.js"
+import grupoComision from "./src/routes/grupoComision.routes.js"
+import solicitud from "./src/routes/solicitud.routes.js"
 
 //Crear aplicación express
 const App = express();
@@ -14,5 +20,16 @@ App.use(cors())
 //Trar end-points que esten disponible en la ruta localhost:3000/api/
 App.use("/api",routerProyecto);
 
+App.use("/api",comision);
+
+App.use("/api",disponibilidad);
+
+App.use("/api",estudiantes);
+
+App.use("/api",examen);
+
+App.use("/api",grupoComision);
+
+App.use("/api",solicitud);
 
 App.listen(3000);
